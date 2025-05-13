0d8afecb3c81f128730b5e8f4a1302c9f5a283a3
caioc3po <caioc3po@gmail.com>
2025-05-13 18:30:30 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/caioc3po.png" />
+    <Titulo fontSize={20}>Caio Souza</Titulo>
+  </aside>
+)
+
+export default Sidebar
