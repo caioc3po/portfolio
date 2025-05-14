09f601720df084805e44c9cdb6cf311eb9596a56
caioc3po <caioc3po@gmail.com>
2025-05-14 15:10:34 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/caioc3po.png" />
-    <Titulo fontSize={20}>Caio Souza</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Caio Souza</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        caioc3po
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
