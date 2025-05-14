6a01c9b2bb6245b334b972431ac4eedc4e80688c
caioc3po <caioc3po@gmail.com>
2025-05-14 17:40:52 -0300

    adicionando a funcionalidade de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import Sidebar from './containers/Sidebar'
-import EstiloGlobal, { Container } from './styles'
-import Sobre from './containers/Sobre'
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
+
 import Projetos from './containers/Projetos'
+import Sidebar from './containers/Sidebar'
+import Sobre from './containers/Sobre'
+import EstiloGlobal, { Container } from './styles'
+import temaDark from './themes/dark'
+import temaLight from './themes/light'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
 
+import { Theme } from '../../themes/light'
+
 import { Props } from '.'
 
 export const P = styled.p<Props>`
-  font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
-  color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
+  font-size: 14px;
+  color: ${(props) =>
+    props.tipo === 'principal'
+      ? (props.theme as Theme).corPrincipal
+      : (props.theme as Theme).corSecundaria};
   line-height: 22px;
 `
